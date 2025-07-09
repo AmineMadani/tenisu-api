1c3749034f4669b6f62cc1876e189934abf99ed8
Aminemad99 <aminemadani1999@gmail.com>
2025-07-10 00:06:27 +0200

    feat: Tenisu-api project initialization using Spring Boot & Java 21

---
+++ src/main/java/com/latelier/tenisu_api/TenisuApiApplication.java
@@ -0,0 +1,13 @@
+package com.latelier.tenisu_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenisuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenisuApiApplication.class, args);
+	}
+
+}
