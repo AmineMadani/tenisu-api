12d68ce6ca2a1bb13943fb1955eec0653adc553a
AmineMadani <madani.amine1999@gmail.com>
2025-07-10 21:26:58 +0200

    feat: Adding Model classes based on the JSON data

---
+++ src/main/java/com/latelier/tenisu_api/model/Country.java
@@ -0,0 +1,22 @@
+package com.latelier.tenisu_api.model;
+
+public class Country {
+    private String code;
+    private String picture;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+}
---
+++ src/main/java/com/latelier/tenisu_api/model/Player.java
@@ -0,0 +1,94 @@
+package com.latelier.tenisu_api.model;
+
+public class Player {
+    private String id;
+    private String firstname;
+    private String lastname;
+    private String shortname;
+    private String sex;
+    private int age;
+    private int height;
+    private int weight;
+    private Country country;
+    private PlayerData playerData;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getShortname() {
+        return shortname;
+    }
+
+    public void setShortname(String shortname) {
+        this.shortname = shortname;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public PlayerData getPlayerData() {
+        return playerData;
+    }
+
+    public void setPlayerData(PlayerData playerData) {
+        this.playerData = playerData;
+    }
+}
